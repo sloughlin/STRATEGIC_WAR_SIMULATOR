2f572d41e9597c4687067a6b363d8c699a9f8acf
aowsenek <aowsenek@gmail.com>
2018-12-10 18:50:54 -0700

    Chess subscriber/listener implemented.

--- chess/stockfishInterface.py
+++ chess/stockfishInterface.py
@@ -1,12 +1,18 @@
-import rospy
+#import rospy
+from std_msgs.msg import Int16MultiArray
+from chess_bot.srv import *
 import chess
 import chess.uci
 import numpy as np
 
+#---------Globals--------------------------------------------#
+game, stockfish, old_bg, time = None #Uninitialized
+
+#---------Debug----------------------------------------------#
 def print_(data):
     for i in range(7,-1,-1):
         print(data[i*8:i*8+8])
-#--------------------------------------------
+
 def recieve_msg(old_bg):
     notation = input("Input Move:")
     s,e = convert_notation_to_index(notation)
@@ -15,15 +21,18 @@ def recieve_msg(old_bg):
     new_bg[e[0]+(e[1]-1)*8] = 1
     return new_bg
 
-def listener():
-    rospy.init_node('chess_listener')
-    rospy.Subscriber('chess_board',bg,callback)
-
 def send_msg(notation,new_bg,flags=None):
     s,e = convert_notation_to_index(notation)
     new_bg[s[0]+(s[1]-1)*8] = 0
     new_bg[e[0]+(e[1]-1)*8] = 2
     print_(new_bg)
+    return new_bg
+
+#---------Utilities------------------------------------------#
+def update_board(old_bg,s,e):
+    new_bg = old_bg.copy()
+    new_bg[s[0]+(s[1]-1)*8] = 0
+    new_bg[e[0]+(e[1]-1)*8] = 1
     return new_bg
 
 def convert_index_to_notation(start,end):
@@ -45,15 +54,29 @@ def convert_notation_to_index(notation):
 def convert_data_to_move(old_bg,new_bg):
     new_bg = np.array(new_bg)
     old_bg = np.array(old_bg)
-    
     start = np.where(old_bg & ~new_bg)
     end   = np.where(new_bg & ~old_bg)
-
     return start,end
 
-def robit_turn(game,stockfish,new_bg):
+#-------Start Game Functions---------------------------------#
+
+def ros_publisher(startx,starty,endx,endy,
+                                        is_capture=False,
+                                        is_pessant=False,
+                                        is_promotion=False,
+                                        is_casle_left=False,
+                                        is_casle_right=False, 
+                                        errorCode = None):
+    pub = rospy.Publisher("chess_piece_move",ChessPieceMove)
+    rospy.init_node('chess_piece_node',anonymous=True)
+    msg = ChessPieceMove
+    msg.whatever = stuff#todo
+
+    pub.publish(msg)
+
+def robit_turn(game,stockfish,new_bg,time):
     stockfish.position(game)
-    res = stockfish.go()
+    res = stockfish.go(btime = time[0], wtime = time[1]) #Will time be in M:S or milliseconds. Needs to be passed as ms
     game.push(res.bestmove)
     print("Robit Move:",res.bestmove)
     print(game)
@@ -62,11 +85,12 @@ def robit_turn(game,stockfish,new_bg):
 
 def person_turn(old_bg,new_bg,game):
     s,e = convert_data_to_move(old_bg,new_bg)
+    new_bg = update_board(old_bg,s,e)
     try:
         notation = convert_index_to_notation(s,e)
         print("Person Move:",notation)
     except:
-        new_bg = recieve_msg(old_bg)
+        new_bg = update_board(old_bg)
         game = person_turn(old_bg,new_bg,game)
         return game
     try:
@@ -75,17 +99,25 @@ def person_turn(old_bg,new_bg,game):
         send_msg(notation[2:]+notation[:2],old_bg,"Illegal")
     print_(new_bg)
     print(game)
-    return game
+    return game,new_bg
 
-def start_game():
+def game_loop(data):
+    global game, stockfish, old_bg, time
+    if(not game.is_game_over()):
+        game, new_bg = person_turn(old_bg,game)
+        game, new_bg = robit_turn(game,stockfish,new_bg,time)
+        old_bg = new_bg.copy()
+
+def globalise_time(data):
+    global time
+    time = (data[0],data[1])#btime, wtime
+
+def main():
+    global game, stockfish, old_bg
     game = chess.Board()
     stockfish = chess.uci.popen_engine("stockfish")
     stockfish.uci()
     stockfish.setoption({"skill level": 0})
-    return game,stockfish
-
-def main():
-    game,stockfish = start_game()
     old_bg = [1,1,1,1,1,1,1,1,
               1,1,1,1,1,1,1,1,
               0,0,0,0,0,0,0,0,
@@ -94,12 +126,12 @@ def main():
               0,0,0,0,0,0,0,0,
               2,2,2,2,2,2,2,2,
               2,2,2,2,2,2,2,2]
+    ros_recieve_msg()
 
-    while(not game.is_game_over()):
-        bg = recieve_msg(old_bg)
-        game = person_turn(old_bg,bg,game)
-        game, bg = robit_turn(game,stockfish,bg)
-        old_bg = bg.copy()
-
+def ros_listener():
+    rospy.init_node("ros_recieve_msg",anonymous=True)
+    rospy.Subscriber("time",Int32, globalise_time) #To be determined
+    rospy.Subscriber("board_state",Int16MultiArray,game_loop)
+    rospy.spin()
 
 if __name__ == '__main__': main()
